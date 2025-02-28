80c10866388f7ef88f84ebdef7512b78dd05cc34
Pedro Munhoz Rosin <pedromunhozrosin@gmail.com>
2025-02-28 15:21:28 -0300

    AAAAI
    AAAAI

--- Spacer vs Invaders/Assets/GameFolder/Scripts/PlayerController.cs
+++ Spacer vs Invaders/Assets/GameFolder/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     private float boundX = 10.5f;
     private Rigidbody2D rigidbody;
+    //parte de tiro que vi num tutorial:
+    public GameObject ProjectilePrefab;
+    public Transform LaunchOffset;
+    public float fireRate = 0.5f; // Taxa de tiro (em segundos)
+    private float nextFireTime = 0f;
 
     void Start()
     {
@@ -39,8 +44,14 @@ public class PlayerController : MonoBehaviour
         transform.position = pos;
 
         //PARTE DE TIROOOOO:
-        if(Input.GetKey(KeyCode.Space)){
-            Debug.Log("a");
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFireTime)
+        {
+            // Instancia o projétil na posição do LaunchOffset com a rotação do player
+            Instantiate(ProjectilePrefab, LaunchOffset.position, LaunchOffset.rotation);
+
+            // Atualiza o tempo do próximo disparo
+            nextFireTime = Time.time + fireRate;
         }
+
     }
 }
