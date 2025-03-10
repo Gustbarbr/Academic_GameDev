5da717df159957380fe397e83264f3bb4bd6b3cf
Gustbarbr <gustbarbr@gmail.com>
2025-03-10 16:21:30 -0300

    Adiciona morte do inimigo

--- A Place Between/Assets/Game Folder/Scripts/PlayerControl.cs
+++ A Place Between/Assets/Game Folder/Scripts/PlayerControl.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Movimentação
+    // Movimentacao
     [HideInInspector]
     public float movementSpeed = 5f;
     Rigidbody2D rb;
 
-    // Variável para capturar o objeto "Lantern"
+    // Variavel para capturar o objeto "Lantern"
     public Transform lantern;
 
     // Ataque
@@ -21,29 +21,29 @@ public class PlayerControl : MonoBehaviour
 
     void Start()
     {
-        // Guarda o componente RigidBody2D na variável
+        // Guarda o componente RigidBody2D na variavel
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        // Captura a posição do mouse
+        // Captura a posicao do mouse
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = (new Vector2(mousePosition.x, mousePosition.y) - (Vector2)transform.position).normalized;
 
-        // Atualiza a lanterna para apontar para a direção do mouse
+        // Atualiza a lanterna para apontar para a direcao do mouse
         lantern.up = direction;
 
         // Movimenta o jogador
         MovePlayer();
 
-        // Define o botão de ataque
+        // Define o botao de ataque
         bool fire = Input.GetKey(KeyCode.Mouse0);
 
         // Atualiza o temporizador de ataque
         attackTimer += Time.deltaTime;
 
-        // Se o botão for pressionado e o tempo de recarga já passou, chama a função de disparo
+        // Se o botao for pressionado e o tempo de recarga ja passou, chama a funcao de disparo
         if (fire && attackTimer >= attackCooldown)
         {
             Fire();
@@ -62,16 +62,16 @@ public class PlayerControl : MonoBehaviour
 
     void Fire()
     {
-        // Instancia a bala na posição do jogador (transform.position)
+        // Instancia a bala na posicao do jogador (transform.position)
         Rigidbody2D bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as Rigidbody2D;
 
-        // Obtém a posição do mouse no mundo
+        // Obtem a posicao do mouse no mundo
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Calcula a direção entre o jogador e o mouse
+        // Calcula a direcao entre o jogador e o mouse
         Vector2 fireDirection = (new Vector2(mousePosition.x, mousePosition.y) - (Vector2)transform.position).normalized;
 
-        // Ajusta a velocidade diretamente na direção do mouse
+        // Ajusta a velocidade diretamente na direcao do mouse
         bullet.velocity = fireDirection * projectileSpeed;  // Movimenta a bala com a velocidade máxima na direção do mouse
 
         // Reseta o tempo de recarga do ataque
