6b557c414d6d628c7cd2c24e641303e1ed99f246
Gustavo Gomes Barbosa <gustbarbr@gmail.com>
2025-03-21 15:17:25 -0300

    Adicionando movimento e pulo

--- Platform/Assets/GameFolder/Scripts/PlayerControl.cs
+++ Platform/Assets/GameFolder/Scripts/PlayerControl.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     Rigidbody2D rb;
+    private Animator animator;
+
+    public float moveSpeed;
+    public float jumpForce;
 
     void Start(){
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update(){
         playerMovement();
+        playerJump();
     }
 
     void playerMovement(){
+        // Armazena o valor da movimentação através das teclas direcionais
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2 (horizontalMovement * moveSpeed, transform.position.y);
+        // Aplica a velocidade no rigidbody a partir da multiplicação entre o input horizontal e o valor da movespeed
+        rb.velocity = new Vector2 (horizontalMovement * moveSpeed, rb.velocity.y);
+
+        // Basicamente é só para ativar a animação de andar (animator) e mudar a direção que ele está olhando
+        if(horizontalMovement > 0){
+            transform.localScale = new Vector3(1.769f, 1.769f, 1.769f);
+            animator.SetFloat("Speed", 1);
+        }
+        else if(horizontalMovement < 0){
+            transform.localScale = new Vector3(-1.769f, 1.769f, 1.769f);
+            animator.SetFloat("Speed", 1);
+        }
+
+        else{
+            animator.SetFloat("Speed", 0);
+        }
+    }
+
+    void playerJump(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
